cca14da4d983507b539187c436f5e1f71679857b
Chandler Mayo <chandler@chandlermayo.com>
2016-03-16 15:28:07 -0700

    Made it work in Ubuntu

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -1,11 +1,14 @@
 char *string_ncopy(char *dest, const char *src, int n)
-{
-	int count = 0;
-	while (count < n) {
-		*dest = *src;
-		src++;
-		dest++;
-		count++;
-	}
-	return &(dest[-count]);
-}
+  {
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++) 
+    {
+      dest[i] = src[i];
+    }
+    for ( ; i < n; i++) 
+    {
+      dest[i] = '\0';
+    }
+	return dest;
+  }
